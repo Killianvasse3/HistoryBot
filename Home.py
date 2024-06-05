f4bd1ca07646efa2641c1ed99b26b0737086c374
MAS leandra <72005705+leandramas@users.noreply.github.com>
2024-06-05 10:20:17 +0200

    MAJ Phrase accueil

--- Home.py
+++ Home.py
@@ -9,7 +9,7 @@ st.set_page_config(
 
 st.title(Config.PAGE_TITLE)
 
-st.markdown("Découvrez l'histoire de manière interactive avec notre bot ! Testez vos connaissances et apprenez grâce à des quiz historiques engageants.")
+st.markdown(" Votre ami d'apprentissage interactif ! Plongez dans l'histoire, testez vos connaissances avec des quiz captivants et découvrez de nouvelles perspectives à chaque question. Transformez vos révisions en une aventure passionnante !")
 st.markdown("""\n""")
 
 with st.sidebar:   
