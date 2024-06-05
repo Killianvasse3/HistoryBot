f6adbe550d2a95dc5c29745f0189aaba6b2e7762
killian vasse <killian.vasse@epitech.digital>
2024-06-05 11:15:29 +0200

    Add folder Page and page Hitoire & Geo

---
+++ Page/HistoireBot.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from config import Config
+from helpers.llm_helper import chat, stream_parser
+
+st.set_page_config(
+    page_title=Config.PAGE_TITLE,
+    initial_sidebar_state="expanded"
+)
+
+st.title(Config.PAGE_TITLE)
+
+st.markdown(" Votre ami d'apprentissage interactif ! Plongez dans l'histoire, testez vos connaissances avec des quiz captivants et découvrez de nouvelles perspectives à chaque question. Transformez vos révisions en une aventure passionnante !")
+st.markdown("""\n""")
+
+with st.sidebar:   
+    st.markdown("# Chat Options")
+    model = st.selectbox('What model would you like to use?', Config.OLLAMA_MODELS)
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if user_prompt := st.chat_input("What would you like to ask?"):
+    with st.chat_message("user"):
+        st.markdown(user_prompt)
+
+    st.session_state.messages.append({"role": "user", "content": user_prompt})
+
+    with st.spinner('Generating response...'):
+        llm_stream = chat(user_prompt, model=model, history=st.session_state.messages)
+
+        stream_output = ""
+        for output in stream_parser(llm_stream):
+            stream_output = output
+        
+        with st.chat_message("assistant"):
+            st.markdown(stream_output)
+        
+        st.session_state.messages.append({"role": "assistant", "content": stream_output})
